04e2bb61528b289730816caf5519b582e4a6b2c7
Erfan Mirshams <erfanmirshams@protonmail.com>
2023-07-22 23:52:15 +0330

    add pywal backgrounds and prettify extensively

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
 #define XF86AudioMute 0x1008ff12
 
 /* appearance */
-static const unsigned int borderpx = 3; /* border pixel of windows */
-static const unsigned int gappx = 5;    /* gaps between windows */
+static const unsigned int borderpx = 7; /* border pixel of windows */
+static const unsigned int gappx = 6;    /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const unsigned int systraypinning =
     0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
@@ -20,9 +20,8 @@ static const int systraypinningfailfirst =
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
-static char font[]            = "DejavuSansMono:size=10";
-static const char *fonts[] = {font,
-                              "NotoColorEmoji:pixelsize=25",
+static char font[] = "DejavuSansMono:size=10";
+static const char *fonts[] = {font, "NotoColorEmoji:pixelsize=25",
                               "fontawesome-webfont:pixelsize=32"};
 static const char dmenufont[] = "DejavuSansMono:size=10";
 static char normbgcolor[] = "#222222";
@@ -83,9 +82,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-c",           "-l",  "5",         "-m",  dmenumon,
-    "-fn",       dmenufont,      "-nb", normbgcolor, "-nf", normfgcolor,
-    "-sb",       selbordercolor, "-sf", selfgcolor,  NULL};
+    "dmenu_run", "-c",       "-l",  "5",         "-m",  dmenumon,
+    "-fn",       dmenufont,  "-nb", normbgcolor, "-nf", normfgcolor,
+    "-sb",       selbgcolor, "-sf", selfgcolor,  NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *normie_browser[] = {"firefox", "-p", "normie-browsing",
                                        NULL};
@@ -106,21 +105,21 @@ static const char *screen_shot_utility[] = {"flameshot", "launcher", NULL};
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		{ "font",               STRING,  &font },
-		{ "dmenufont",          STRING,  &dmenufont },
-		{ "normbgcolor",        STRING,  &normbgcolor },
-		{ "normbordercolor",    STRING,  &normbordercolor },
-		{ "normfgcolor",        STRING,  &normfgcolor },
-		{ "selbgcolor",         STRING,  &selbgcolor },
-		{ "selbordercolor",     STRING,  &selbordercolor },
-		{ "selfgcolor",         STRING,  &selfgcolor },
-		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",          		INTEGER, &snap },
-		{ "showbar",          	INTEGER, &showbar },
-		{ "topbar",          	INTEGER, &topbar },
-		{ "nmaster",          	INTEGER, &nmaster },
-		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",      	 	FLOAT,   &mfact },
+    {"font", STRING, &font},
+    {"dmenufont", STRING, &dmenufont},
+    {"color0", STRING, &normbgcolor},
+    {"color8", STRING, &normbordercolor},
+    {"color7", STRING, &normfgcolor},
+    {"color2", STRING, &selbgcolor},
+    {"color3", STRING, &selbordercolor},
+    {"color7", STRING, &selfgcolor},
+    {"borderpx", INTEGER, &borderpx},
+    {"snap", INTEGER, &snap},
+    {"showbar", INTEGER, &showbar},
+    {"topbar", INTEGER, &topbar},
+    {"nmaster", INTEGER, &nmaster},
+    {"resizehints", INTEGER, &resizehints},
+    {"mfact", FLOAT, &mfact},
 };
 
 static const Key keys[] = {
@@ -166,6 +165,14 @@ static const Key keys[] = {
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+};
+
+/* signal definitions */
+/* signum must be greater than 0 */
+/* trigger signals using `xsetroot -name "fsignal:<signum>"` */
+static Signal signals[] = {
+    /* signum       function        argument  */
+    {1, reload_xresources, {.v = NULL}},
 };
 
 /* button definitions */
